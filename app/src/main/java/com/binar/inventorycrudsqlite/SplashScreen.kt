21fcb169b95c70b8ac326e033eae4a5f9ec515cc
Hanif yogatama <hanifyotama@gmail.com>
2020-08-10 18:13:01 +0700

    Initial commit

---
+++ app/src/main/java/com/binar/inventorycrudsqlite/SplashScreen.kt
@@ -0,0 +1,26 @@
+package com.binar.inventorycrudsqlite
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import java.lang.NullPointerException
+
+class SplashScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+        try {
+            this.supportActionBar?.hide()
+        } catch (e: NullPointerException) {
+        }
+
+        Handler().postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 5000)
+
+    }
+}
